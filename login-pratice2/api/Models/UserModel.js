311a79254331d27663847ddf25ff3d3dd3f2593d
prakash-2002 <prakashslm27@gmail.com>
2024-12-15 12:38:20 +0530

    db connection edited user control created usermodel edited

--- login-pratice2/api/Models/UserModel.js
+++ login-pratice2/api/Models/UserModel.js
@@ -3,18 +3,22 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     username: {
         type:String,
-        require: [true, "name is require"]
+        required: [true, "name is require"]
     },
     email:{
         type:String,
-        require:[true, "email is required"]
+        required:[true, "email is required"]
     },
     password:{
         type:String,
-        require:[true, "password is required"]
+        required:[true, "password is required"]
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now()
     }
 
 })
 
 const userModel=mongoose.model("users",userSchema);
-module.exports=userModel;
+module.exports=userModel
--- login-pratice2/api/config/databaseConnection.js
+++ login-pratice2/api/config/databaseConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDatabase = () => {
-    mongoose.connect("mongodb://localhost:27017/interviewlogincheck/loginPratice2").then(
+    mongoose.connect("mongodb://localhost:27017/loginPratice2").then(
         (con) => console.log(`mongodb is running on host ${con.connection.host} in PORT ${con.connection.port}`)).catch((err)=>{
             console.log(err);
         })
