871af67664a64aacf8578faf8432c965abd1da57
prakash-2002 <prakashslm27@gmail.com>
2024-12-15 10:45:21 +0530

    server is set and db is connected

--- login-pratice2/api/server.js
+++ login-pratice2/api/server.js
@@ -1,13 +1,17 @@
-const express= require("express");
-const donenv=require("dotenv");
-const path=require("path");
-donenv.config({path:path.join(__dirname,"config","config.env")})
+const express = require("express");
+const donenv = require("dotenv");
+const path = require("path");
+const connectDatabase = require("./config/databaseConnection");
+donenv.config({ path: path.join(__dirname, "config", "config.env") })
 
-const server=express();
+connectDatabase();
+const server = express();
+server.use(express.json());
+const user = require("./routes/user");
+
+server.use("/api/auth/", user);
 
 
-
-
-server.listen(process.env.PORT,()=>{
+server.listen(process.env.PORT, () => {
     console.log(`server is running on ${process.env.PORT}`)
 })
