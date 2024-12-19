c784a1fc0b5f1752c92339be96e91e972a5348e9
prakash-2002 <prakashslm27@gmail.com>
2024-12-19 17:57:23 +0530

    get route added

--- login-pratice2/api/routes/user.js
+++ login-pratice2/api/routes/user.js
@@ -1,7 +1,9 @@
 const express=require("express");
-const { addUser } = require("../controllers/userController");
+const { addUser, getUsers } = require("../controllers/userController");
 const router=express.Router();
 
 router.route("/adduser").post(addUser)
+router.route("/users").get(getUsers)
+
 
 module.exports=router;
